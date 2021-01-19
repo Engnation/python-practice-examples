6e3a6250de780d3c46b4e809d6cdb11912579200
Ryan Schmalenberg <engnation101@gmail.com>
2021-01-18 22:31:24 -0500

    initial commit

---
+++ Dictionaries/Dictionary_Functions.py
@@ -0,0 +1,13 @@
+#The following example is adapted from SOLOLEARN:
+
+squares = {1:1,2:4,3:"error",4:16}
+squares[8] = 64
+
+print(squares[3])
+
+#Dictionary key values can be re-assigned:
+squares[3] = 9
+
+print(squares[3])
+
+print(squares)
