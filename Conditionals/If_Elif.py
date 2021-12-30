9a45c5eab992ea66361e06165a4a94ec83875f7b
Ryan Schmalenberg <engnation101@gmail.com>
2021-12-30 15:29:32 -0500

    intial commit

---
+++ Conditionals/If_Elif.py
@@ -0,0 +1,17 @@
+a = 100
+b = 200
+x = 1
+y = 2
+
+#A quick concrete reminder if vs. elif in Python
+
+if a == 100:
+    print("Yes, a = 100")
+elif b == 200:
+    print("Yes, b = 200")
+
+if x == 1:
+    print("Yes, x = 1")
+if y == 2:
+    print("Yes, y = 2")
+
