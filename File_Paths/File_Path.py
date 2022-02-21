12138a3018a9047f468c08f145ce34545e8cce37
Ryan Schmalenberg <engnation101@gmail.com>
2022-02-20 22:05:35 -0500

    Update File_Path.py

--- File_Paths/File_Path.py
+++ File_Paths/File_Path.py
@@ -10,8 +10,9 @@ f.close()
 folder_name = "subfolder"
 if not os.path.isdir(folder_name):
     os.mkdir(folder_name)
-#do whatever and send to this folder:  os.path.join(folder_name)
+# set destination file in subdirectory 
 destination = os.path.join(folder_name, f"destination.txt")
+# write to subdirectory
 file2 = open(destination,"w")
 file2.write(content)
 file2.close()
