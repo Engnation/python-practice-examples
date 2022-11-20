629ab0f6b8d676cbb44abc46a61e972637aab563
Ryan Schmalenberg <engnation101@gmail.com>
2022-11-20 10:03:48 -0500

    added list comprehension

--- Tuples/index_into_list_of_tuples.py
+++ Tuples/index_into_list_of_tuples.py
@@ -1,3 +1,10 @@
 list_of_tuples = [((20,33),(33,42),1),((22,23),(33,43),2),((25,28),(27,29),3),((21,30),(32,39),4)]
 
+print(list_of_tuples[0][2])
 print(list_of_tuples[1][2])
+print(list_of_tuples[2][2])
+print(list_of_tuples[3][2])
+
+desired_elements = [x[2] for x in list_of_tuples]
+
+print(desired_elements)
